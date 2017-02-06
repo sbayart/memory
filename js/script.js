ad1be3ef0f6ece629a77103e9ab03b7e54692d51
solene <solenantoine@gmail.com>
2017-02-06 17:11:42 +0100

    Les cartes se retournent (toutes...)!!!!

--- js/script.js
+++ js/script.js
@@ -5,9 +5,20 @@ var boardElement = document.getElementById("board");
 for (var j = 0; j<2; j++) {
     for (var i = 0; i < cardsNumber/2; i++) {
         var cardElement = document.createElement("div");
-        cardElement.innerHTML = i;
         cardElement.className = "card";
         boardElement.appendChild(cardElement);
-    }
+
+        var cardValeurElement = document.createElement("div");
+        cardValeurElement.innerHTML = i;
+        cardValeurElement.className = "valeur";
+        cardElement.appendChild(cardValeurElement);
+
+        var cardCacheElement = document.createElement("div");
+        cardCacheElement.className = "masque";
+        cardElement.appendChild(cardCacheElement);
+
+        cardCacheElement.addEventListener("click", function(){
+            this.style.display="none";
+    });
 }
-i.addEventListener("click",changeClass)
+}
