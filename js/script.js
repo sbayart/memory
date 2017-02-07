a0700421a30a976fb5b6fae5d9474909b2aa28f9
Sylvie <sylvie.veyres@gmail.com>
2017-02-07 10:55:41 +0100

    on retourne 2 cartes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ var cardsNumber = 12;
 
 var boardElement = document.getElementById("board");
 
+var cardRetournee1 = null;
+var cardRetournee2 = null;
+
 for (var j = 0; j<2; j++) {
     for (var i = 0; i < cardsNumber/2; i++) {
         var cardElement = document.createElement("div");
@@ -18,7 +21,16 @@ for (var j = 0; j<2; j++) {
         cardElement.appendChild(cardCacheElement);
 
         cardCacheElement.addEventListener("click", function(){
-            this.style.display="none";
-    });
-}
+            if((cardRetournee1==null)&&(cardRetournee2==null)){
+                this.style.display="none";
+                cardRetournee1=i;
+            }else if ((cardRetournee1!==null)&&(cardRetournee2==null)) {
+                this.style.display="none";
+                cardRetournee2=i;
+            }else if ((cardRetournee1!==null)&&(cardRetournee2!==null)) {
+                this.style.display="block";
+            }
+        });
+
+    }
 }
