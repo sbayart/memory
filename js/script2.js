6d2994bb891f514065645642f5a5184561d8c2aa
Sylvie <sylvie.veyres@gmail.com>
2017-02-24 11:33:46 +0100

    memory qui fonctionne même en jquery

--- js/script2.js
+++ js/script2.js
@@ -1,50 +1,47 @@
-var cardsNumber = 12;
+$(document).ready(function(){
+    var cardsNumber = 12;
+    var cardRetournee1 = null;
+    var cardRetournee2 = null;
 
-var boardElement = document.getElementById("board");
+    var board = $("#board");
+    for (var j = 0; j<2; j++) {
+        for (var i = 0; i < cardsNumber/2; i++) {
+            var card = $('<div class="card"></div>');
+            board.append(card);
+            var valeur = $('<div class="valeur">'+i+'</div>')
+            var masque = $('<div class="masque"></div>');
+            card.append(valeur, masque)            
+        };
+    };
+    $(".masque").click(function(){
+        if(cardRetournee1==null && cardRetournee2==null){
+            $(this).css("display","none");
+            cardRetournee1 = $(this);
+        } else if (cardRetournee1!=null && cardRetournee2==null) {
+            $(this).css("display","none");
+            cardRetournee2 = $(this);
+        } else if (cardRetournee1!=null && cardRetournee2!=null) {
+            var frangin1 = cardRetournee1.prev().html();
+            var frangin2 = cardRetournee2.prev().html();
+            if (frangin1==frangin2) {
+                cardRetournee1=$(this);
+                cardRetournee2=null;
+                $(this).css("display","none");
 
-var cardRetournee1 = null;
-var cardRetournee2 = null;
-
-
-    for (var i = 0; i < cardsNumber; i++) {
-        var cardElement = document.createElement("div");
-        cardElement.className = "card";
-        boardElement.appendChild(cardElement);
-
-        var cardValeurElement = document.createElement("div");
-        cardValeurElement.innerHTML = i;
-        cardValeurElement.className = "valeur";
-        cardElement.appendChild(cardValeurElement);
-
-        var cardCacheElement = document.createElement("div");
-        cardCacheElement.className = "masque";
-        cardElement.appendChild(cardCacheElement);
-
-
-
-        cardCacheElement.addEventListener("click", function(){
-            if(cardRetournee1==null && cardRetournee2==null){
-                this.style.display="none";
-                cardRetournee1 = this;
-            } else if (cardRetournee1!=null && cardRetournee2==null) {
-                this.style.display="none";
-                cardRetournee2 = this;
-            } else if (cardRetournee1!=null && cardRetournee2!=null) {
-                var frangin1 = cardRetournee1.previousSibling.innerHTML;
-                var frangin2 = cardRetournee2.previousSibling.innerHTML;
-                if (frangin1==frangin2) {
-                    cardRetournee1=this;
-                    cardRetournee2=null;
-                    this.style.display="none";
-
-                } else {
-                    cardRetournee1.style.display="block";
-                    cardRetournee2.style.display="block";
-                    cardRetournee1=this;
-                    cardRetournee2=null;
-                    this.style.display="none";
-                }
-            }
-        });
-
-    }
+            } else {
+                cardRetournee1.css("display","block");
+                cardRetournee2.css("display","block");
+                cardRetournee1=$(this);
+                cardRetournee2=null;
+                $(this).css("display","none");
+            };
+        };
+    });
+    $(function () {
+        var parent = $(".shuffle");
+        var divs = parent.children();
+        while (divs.length) {
+            parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+        }
+    });
+})
